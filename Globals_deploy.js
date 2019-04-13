3ea7c4cf1d00bd4fcafb3b92cffdffd1dd757db4
alinso <soyaslanaliinsan@gmail.com>
2019-04-13 10:44:09 +0300

    auto deploy2

--- Globals_deploy.js
+++
@@ -1,8 +0,0 @@
-class  Globals{
-
-    static serviceUrl="https://activityfriend.net:8443/api/";
-    //static serviceUrl = "http://localhost:8080/";
-    static  adminUrl="sdklsdf/"
-}
-
-export default Globals;
